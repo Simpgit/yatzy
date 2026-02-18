257a30cceda2a8582a1b066bba9bd70c9801edfc
Gyldigt <Andershoffmann@gmail.com>
2026-02-18 13:46:52 +0100

    start dom

---
+++ DOM.js
@@ -0,0 +1,16 @@
+const diceElements = 
+    [
+    document.getElementById("dice1"),
+    document.getElementById("dice2"),
+    document.getElementById("dice3"),
+    document.getElementById("dice4"),
+    document.getElementById("dice5"), 
+]
+
+const rollBtn = document.getElementById("roll-button")
+
+const throwstext = document.getElementById("throws")
+
+console.log(diceElements);
+console.log(rollBtn);
+console.log(throwstext);
